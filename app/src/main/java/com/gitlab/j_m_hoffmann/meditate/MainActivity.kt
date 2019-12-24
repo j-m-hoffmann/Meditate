191b3034a640193c8d4d8fefba4889d4d072d121
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2019-12-24 23:01:11 +0100

    fix: comment out actionbar to avoid crash

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/MainActivity.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/MainActivity.kt
@@ -3,8 +3,6 @@ package com.gitlab.j_m_hoffmann.meditate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -18,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
 
+/*
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -27,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+*/
 
         navBar.setupWithNavController(navController)
     }
