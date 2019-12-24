6b06dc721f6c7039ff19847f6e57638091625255
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2019-12-24 17:12:46 +0100

    style: formatting & optimize imports

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/timer/TimerFragment.kt
@@ -1,12 +1,11 @@
 package com.gitlab.j_m_hoffmann.meditate.ui.timer
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.gitlab.j_m_hoffmann.meditate.R
 
 class TimerFragment : Fragment() {
