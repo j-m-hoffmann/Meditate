9e4494479e7150abf852d66cc943e5965f223648
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2019-12-29 15:28:43 +0100

    feat: connect layout with viewModel via data binding

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/progress/ProgressFragment.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/progress/ProgressFragment.kt
@@ -6,23 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.gitlab.j_m_hoffmann.meditate.R
+import com.gitlab.j_m_hoffmann.meditate.databinding.ProgressFragmentBinding
 
 class ProgressFragment : Fragment() {
 
-    private lateinit var viewModel: ProgressViewModel
+    private lateinit var binding: ProgressFragmentBinding
+
+    private lateinit var progressViewModel: ProgressViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.progress_fragment, container, false)
+        binding = ProgressFragmentBinding.inflate(inflater)
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProgressViewModel::class.java)
-        // TODO: Use the ViewModel
+        progressViewModel = ViewModelProvider(this)[ProgressViewModel::class.java]
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = progressViewModel
+        }
     }
 
 }
