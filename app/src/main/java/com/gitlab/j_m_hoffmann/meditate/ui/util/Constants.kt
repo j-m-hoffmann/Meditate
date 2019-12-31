f0715670aebc9ba5a40d6c8113bc6800a0ff20dc
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2019-12-31 11:32:02 +0100

    fix: ensure hour value is correct

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/util/Constants.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/util/Constants.kt
@@ -1,5 +1,6 @@
 package com.gitlab.j_m_hoffmann.meditate.ui.util
 
-const val hour: Long = 360_000L
-const val minute: Long = 60_000L
 const val second: Long = 1_000L
+const val minute: Long = 60 * second
+const val hour: Long = 60 * minute
+const val day: Long = 24 * hour
