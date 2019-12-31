950f99297deb497de6060694b861770663f8817a
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2019-12-31 10:01:35 +0100

    feat: add ProgressViewModel and bindings

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/progress/ProgressViewModel.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/progress/ProgressViewModel.kt
@@ -1,8 +1,36 @@
 package com.gitlab.j_m_hoffmann.meditate.ui.progress
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.gitlab.j_m_hoffmann.meditate.db.Dao
+import kotlinx.coroutines.launch
 
 class ProgressViewModel(private val dao: Dao) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _countSessions = MutableLiveData<Int>(0)
+    val countSessions: LiveData<Int>
+        get() = _countSessions
+
+    private val _durationAverage = MutableLiveData<Long>(0L)
+    val durationAverage: LiveData<Long>
+        get() = _durationAverage
+
+    private val _durationLongest = MutableLiveData<Long>(0L)
+    val durationLongest: LiveData<Long>
+        get() = _durationLongest
+
+    private val _durationTotal = MutableLiveData<Long>(0L)
+    val durationTotal: LiveData<Long>
+        get() = _durationTotal
+
+    init {
+        viewModelScope.launch {
+            _countSessions.value = dao.countSessions()
+            _durationAverage.value = dao.durationAverage()
+            _durationLongest.value = dao.durationLongest()
+            _durationTotal.value = dao.durationTotal()
+        }
+    }
 }
