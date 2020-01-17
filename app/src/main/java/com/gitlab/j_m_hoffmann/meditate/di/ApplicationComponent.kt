671aef4c15558067416ff3dbd8c968062e512a72
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2020-01-17 10:08:59 +0100

    feat: add application graph

---
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/di/ApplicationComponent.kt
@@ -0,0 +1,28 @@
+package com.gitlab.j_m_hoffmann.meditate.di
+
+import android.content.Context
+import com.gitlab.j_m_hoffmann.meditate.MeditateApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        ProgressModule::class,
+        RepositoryModule::class,
+        SessionModule::class,
+        ViewModelModule::class
+    ]
+)
+interface ApplicationComponent : AndroidInjector<MeditateApplication> {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(@BindsInstance applicationContext: Context): ApplicationComponent
+    }
+}
