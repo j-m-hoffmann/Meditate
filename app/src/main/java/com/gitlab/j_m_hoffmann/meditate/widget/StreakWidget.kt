5ef147a531969b248bf9464a93051013b1e9b4ef
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2020-01-21 11:59:23 +0100

    refactor: fetch values once, outside of the loop

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/widget/StreakWidget.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/widget/StreakWidget.kt
@@ -20,22 +20,23 @@ import com.gitlab.j_m_hoffmann.meditate.util.WIDGET_REQUEST_CODE
 class StreakWidget : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+        val days = preferences.getInt(context.getString(key_streak_value), 0)
+
+        val widgetText = days.toPlural(R.plurals.days_of_meditation, R.string.widget_text_default, context)
+
+        val widgetTextColor =
+            preferences.getString(context.getString(key_widget_color), context.getString(default_widget_color))
+
         for (id in appWidgetIds) {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-
-            val days = preferences.getInt(context.getString(key_streak_value), 0)
-
-            val widgetText = days.toPlural(R.plurals.days_of_meditation, R.string.widget_text_default, context)
-
             val pendingIntent = Intent(context, MainActivity::class.java).let { intent ->
                 PendingIntent.getActivity(context, WIDGET_REQUEST_CODE + id, intent, 0)
             }
 
-            val color =
-                preferences.getString(context.getString(key_widget_color), context.getString(default_widget_color))
-
             val views = RemoteViews(context.packageName, R.layout.widget).apply {
-                setTextColor(widget_text, Color.parseColor(color))
+                setTextColor(widget_text, Color.parseColor(widgetTextColor))
                 setTextViewText(widget_text, widgetText)
                 setOnClickPendingIntent(widget_text, pendingIntent)
             }
