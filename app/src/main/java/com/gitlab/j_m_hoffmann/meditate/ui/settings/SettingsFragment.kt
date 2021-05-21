118d93ebe8a7322a710b9d21ce6e330bd035b900
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2021-05-21 12:06:22 +0200

    refactor: increase clarity

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/ui/settings/SettingsFragment.kt
@@ -6,31 +6,18 @@ import androidx.preference.ListPreference
 import androidx.preference.Preference.OnPreferenceChangeListener
 import androidx.preference.PreferenceFragmentCompat
 import com.gitlab.j_m_hoffmann.meditate.R
-import com.gitlab.j_m_hoffmann.meditate.R.string.key_theme
-import com.gitlab.j_m_hoffmann.meditate.R.string.key_widget_color
 import com.gitlab.j_m_hoffmann.meditate.extensions.updateWidget
 import com.gitlab.j_m_hoffmann.meditate.widget.StreakWidget
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     private var themePreference: ListPreference? = null
-
-    private val themeChangeListener = OnPreferenceChangeListener { _, newTheme ->
-        AppCompatDelegate.setDefaultNightMode((newTheme as String).toInt())
-        true
-    }
-
     private var widgetColorPreference: ListPreference? = null
-
-    private val widgetColorChangeListener = OnPreferenceChangeListener { _, _ ->
-        requireActivity().applicationContext.updateWidget<StreakWidget>()
-        true
-    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        themePreference = findPreference(getString(key_theme))
-        widgetColorPreference = findPreference(getString(key_widget_color))
+        themePreference = findPreference(getString(R.string.key_theme))
+        widgetColorPreference = findPreference(getString(R.string.key_widget_color))
     }
 
     override fun onResume() {
@@ -43,5 +30,15 @@ class SettingsFragment : PreferenceFragmentCompat() {
         super.onPause()
         themePreference?.onPreferenceChangeListener = null
         widgetColorPreference?.onPreferenceChangeListener = null
+    }
+
+    private val themeChangeListener = OnPreferenceChangeListener { _, newTheme ->
+        AppCompatDelegate.setDefaultNightMode((newTheme as String).toInt())
+        true
+    }
+
+    private val widgetColorChangeListener = OnPreferenceChangeListener { _, _ ->
+        requireActivity().applicationContext.updateWidget<StreakWidget>()
+        true
     }
 }
