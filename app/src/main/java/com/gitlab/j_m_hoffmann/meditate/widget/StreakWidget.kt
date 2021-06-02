dd659a8a9fcd9d8749592acecd06d0e76523758b
Johannes Hoffmann <27806916+j-m-hoffmann@users.noreply.github.com>
2021-06-02 14:34:18 +0200

    fix: run only if widgets exist

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/widget/StreakWidget.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/widget/StreakWidget.kt
@@ -22,27 +22,26 @@ class StreakWidget : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
 
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        if (appWidgetIds.isNotEmpty()) {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
 
-        val days = preferences.getInt(context.getString(key_streak_value), 0)
+            val days = preferences.getInt(context.getString(key_streak_value), 0)
 
-        val widgetText = days.toPlural(R.plurals.days_of_meditation, R.string.widget_text_default, context)
+            val widgetText = days.toPlural(R.plurals.days_of_meditation, R.string.widget_text_default, context)
 
-        val widgetTextColor =
-            preferences.getString(context.getString(key_widget_color), context.getString(default_widget_color))
+            val widgetTextColor =
+                preferences.getString(context.getString(key_widget_color), context.getString(default_widget_color))
 
-        for (id in appWidgetIds) {
             val pendingIntent = Intent(context, MainActivity::class.java).let { intent ->
                 PendingIntent.getActivity(context, WIDGET_REQUEST_CODE, intent, 0)
             }
 
-            val views = RemoteViews(context.packageName, R.layout.widget).apply {
+            val streakWidget = RemoteViews(context.packageName, R.layout.widget).apply {
                 setTextColor(widget_text, Color.parseColor(widgetTextColor))
                 setTextViewText(widget_text, widgetText)
                 setOnClickPendingIntent(widget_text, pendingIntent)
             }
-
-            appWidgetManager.updateAppWidget(id, views)
+            appWidgetIds.forEach { appWidgetManager.updateAppWidget(it, streakWidget) }
         }
     }
 }
