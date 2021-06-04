448f7d68e965c68153620a48c78f6a2594ac5a65
Johannes Hoffmann <27806916+j-m-hoffmann@users.noreply.github.com>
2021-06-04 10:27:45 +0200

    fix: hide navBar when session was aborted

--- app/src/main/java/com/gitlab/j_m_hoffmann/meditate/MainActivity.kt
+++ app/src/main/java/com/gitlab/j_m_hoffmann/meditate/MainActivity.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
@@ -14,6 +13,7 @@ import com.gitlab.j_m_hoffmann.meditate.R.string.key_theme
 import com.gitlab.j_m_hoffmann.meditate.databinding.ActivityMainBinding
 import com.gitlab.j_m_hoffmann.meditate.extensions.updateWidget
 import com.gitlab.j_m_hoffmann.meditate.ui.session.SessionViewModel
+import com.gitlab.j_m_hoffmann.meditate.ui.session.State.Aborted
 import com.gitlab.j_m_hoffmann.meditate.ui.session.State.Ended
 import com.gitlab.j_m_hoffmann.meditate.ui.session.State.InProgress
 import com.gitlab.j_m_hoffmann.meditate.widget.StreakWidget
@@ -36,20 +36,21 @@ class MainActivity : AppCompatActivity() {
 
         val nightMode = preferences.getString(getString(key_theme), getString(default_theme))
 
-        AppCompatDelegate.setDefaultNightMode(nightMode!!.toInt())
-
-        sessionViewModel.state.observe(this, Observer { state ->
-            when (state) {
-                InProgress -> {
+        sessionViewModel.state.observe(this) {
+            @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
+            when (it) {
+                Aborted -> {
                     binding.navBar.visibility = View.GONE
-                    Snackbar.make(binding.navBar, R.string.concentrate, Snackbar.LENGTH_LONG).show()
                 }
                 Ended -> {
                     binding.navBar.visibility = View.VISIBLE
                     applicationContext.updateWidget<StreakWidget>()
                 }
-                else -> binding.navBar.visibility = View.VISIBLE
+                InProgress -> {
+                    binding.navBar.visibility = View.GONE
+                    Snackbar.make(binding.navBar, R.string.concentrate, Snackbar.LENGTH_LONG).show()
+                }
             }
-        })
+        }
     }
 }
