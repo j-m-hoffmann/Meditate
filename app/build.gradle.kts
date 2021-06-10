e3a4658c51aae2d5733890202ac7bbd2a8d5bad3
Johannes Hoffmann <27806916+j-m-hoffmann@users.noreply.github.com>
2021-06-10 14:24:04 +0200

    chore: bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.gitlab.j_m_hoffmann.meditate"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         kapt {
